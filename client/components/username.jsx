37a643c57f42e2216d24fb4962f602670c589da3
Matthew Chau <matthewwchau@gmail.com>
2022-12-29 04:24:31 +0000

    Create and render a mockup in this file to replicate the directions, input field, and a next button. #1

--- client/components/username.jsx
+++ client/components/username.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 export default function NewUserName(props) {
   return (
-    <h1>
-      USERNAME TEST
-    </h1>
+    <form>
+      <div className='mt-5'>
+        <label htmlFor='username'>
+          <input name='username' type='text' value='Type a username here' className='user-input' />
+        </label>
+      </div>
+      <div className='mt-5'>
+        <button type='submit' className='next'>NEXT</button>
+      </div>
+    </form>
   );
 }
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-// import HelloWorld from '../components/hello-world';
 import NewUserName from '../components/username';
 
 export default function Home(props) {
   return (
-    <div>
-      <NewUserName />
+    <div className='container-fluid main' >
+      <div className='mt-5 pt-5'>
+        <div className='mt-5 pt-5 text-center'>
+          <div className='row welcome wht-txt'>
+            <p>Welcome to FuzeChat!</p>
+          </div>
+          <div className='instruct wht-txt'>
+            <p>Please enter a username below</p>
+          </div>
+          <div>
+            <NewUserName />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
