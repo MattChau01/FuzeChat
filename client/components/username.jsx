4d4fa8582795402033725b884216f952ac5ca55d
Matthew Chau <matthewwchau@gmail.com>
2022-12-29 00:09:49 +0000

    Create a class NewUserName into the new component. #1

---
+++ client/components/username.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NewUserName(props) {
+  return (
+    <h1>
+      USERNAME TEST
+    </h1>
+  );
+}
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import HelloWorld from '../components/hello-world';
+// import HelloWorld from '../components/hello-world';
+import NewUserName from '../components/username';
 
 export default function Home(props) {
   return (
     <div>
-      <HelloWorld />
+      <NewUserName />
     </div>
   );
 }
