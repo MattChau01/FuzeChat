eae5b234d6e199234075689ac951f7c70a91a132
Matthew Chau <matthewwchau@gmail.com>
2022-12-29 22:50:14 +0000

    Edit GET request to retrieve data from the users table and test out in terminal. #1

--- server/index.js
+++ server/index.js
@@ -3,12 +3,31 @@ const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
 
+const pg = require('pg');
+
+const db = new pg.Pool({
+  connectionString: 'postgresql://dev:dev@localhost/fuzeChat',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 const app = express();
 
 app.use(staticMiddleware);
 
-app.get('/api/hello', (req, res) => {
-  res.json({ hello: 'world' });
+app.get('/api/users', (req, res, next) => {
+  const sql = `
+    select *
+    from "users"
+  `;
+
+  db.query(sql)
+    .then(result => {
+      const usersList = result.rows;
+      res.json(usersList);
+    })
+    .catch(err => next(err));
 });
 
 app.use(errorMiddleware);
