266b3306f3ac6421b6a1a31ce75b2e7ccb11b80d
Matthew Chau <matthewwchau@gmail.com>
2022-12-29 04:29:22 +0000

    Add a constructor, super, and this.state={isClicked: false}. #1

--- client/components/username.jsx
+++ client/components/username.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
-export default function NewUserName(props) {
-  return (
-    <form>
-      <div className='mt-5'>
-        <label htmlFor='username'>
-          <input name='username' type='text' value='Type a username here' className='user-input' />
-        </label>
-      </div>
-      <div className='mt-5'>
-        <button type='submit' className='next'>NEXT</button>
-      </div>
-    </form>
-  );
+export default class NewUserName extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false
+    };
+  }
+
+  render() {
+    return (
+      <form>
+        <div className='mt-5'>
+          <label htmlFor='username'>
+            <input name='username' type='text' value='Type a username here' className='user-input' />
+          </label>
+        </div>
+        <div className='mt-5'>
+          <button type='submit' className='next'>NEXT</button>
+        </div>
+      </form>
+    );
+
+  }
 }
