9bf7c7f03d6e075137804f9f343d8b6e6ad821ea
Matthew Chau <matthewwchau@gmail.com>
2023-01-02 05:29:53 +0000

    Update data.sql to insert data for chatRoom. #2

--- database/data.sql
+++ database/data.sql
@@ -5,3 +5,11 @@ values (
   'UserOne',
   CURRENT_TIMESTAMP
 );
+
+insert into "chatRooms" (
+  "chatRoomName",
+  "chatRoomId")
+  values
+  ('rc1022', '0'),
+  ('lfz2022', '1'),
+  ('zoomuni', '2');
