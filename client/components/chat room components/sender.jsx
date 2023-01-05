36fc14241e7fa6a3c023601a707e2f43175b97cb
Matthew Chau <matthewwchau@gmail.com>
2023-01-05 03:16:12 +0000

    In the Sender component, make sure that the values change when users click and type the input box. #3

--- client/components/chat room components/sender.jsx
+++ client/components/chat room components/sender.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
 export default class Sender extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className='message-area'>
@@ -8,7 +22,7 @@ export default class Sender extends React.Component {
           <div>
             <form>
               <label htmlFor='message' className='text-box'>
-                <input type='text' name='message' value='This is a placeholder for a really really long message' className='message-bar'/>
+                <input type='text' name='message' value={this.state.value} placeholder='Message' className='message-bar' onChange={this.handleChange}/>
                 <button type='submit' className='send'><i className="fa-solid fa-arrow-up send-arrow" /></button>
               </label>
             </form>
