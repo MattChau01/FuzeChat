5a67df894334d4e93c413e5b00cafaa5d5f78ab4
Matthew Chau <matthewwchau@gmail.com>
2023-01-06 18:00:45 +0000

    Update data.sql to upload sample data for a message. #4

--- database/data.sql
+++ database/data.sql
@@ -14,4 +14,8 @@ insert into "chatRooms" (
 
 -- INSERT INTO USERSINCHAT
 insert into "usersInChat" ("userId", "chatRoomId")
-values (1, 3)
+values (1, 3);
+
+-- INSERT INTO MESSAGES
+insert into "messages" ("newMessage", "chatRoomId", "userId")
+values ('this is a test message', 1, 1);
