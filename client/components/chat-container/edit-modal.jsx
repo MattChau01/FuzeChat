69bdc666db2e0e40422481d29984d5cd1e8d52d0
Matthew Chau <matthewwchau@gmail.com>
2023-02-16 05:19:59 +0000

    Create a mockup modal that will look like the wireframe. #8

--- client/components/chat-container/edit-modal.jsx
+++ client/components/chat-container/edit-modal.jsx
@@ -6,12 +6,13 @@ const styles = {
   //   left: 0,
   //   right: 0,
   //   top: 0,
-  //   bottom: 0
+  //   bottom: 0,
+  //   height: '100vh'
 
   //   // backgroundColor: 'rgb(14, 27, 35, 100%)'
   // },
   modalBlock: {
-    backgroundColor: 'rgb(14, 27, 35, 100%)',
+    backgroundColor: '#283C46',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
@@ -23,11 +24,28 @@ const styles = {
     left: '15%',
     // right: 0
     borderRadius: '0.8rem'
-
   },
-  overlay: {
-    backgroundColor: 'rgb(0, 0, 0, 70 %)'
+  buttonSize: {
+    width: '70%',
+    marginLeft: '1rem'
   }
+  // cancel: {
+  //   backgroundColor: '#EB8E8E',
+  //   borderStyle: 'none',
+  //   borderRadius: '5rem',
+  //   outline: 'none',
+  //   marginLeft: '.5rem'
+  // },
+  // confirm: {
+  //   backgroundColor: '#87EF76',
+  //   borderStyle: 'none',
+  //   borderRadius: '5rem',
+  //   outline: 'none',
+  //   marginLeft: '.5rem'
+  // }
+  // overlay: {
+  //   backgroundColor: 'rgb(0, 0, 0, 70 %)'
+  // }
 };
 
 export function EditModal(props) {
@@ -35,41 +53,65 @@ export function EditModal(props) {
   // ***** Working on styling modal and overlay *****
 
   // need to add `setMessage` as second parameter
-  const [message] = useState('');
+  const [message, setMessage] = useState('');
 
   return (
-    <>
-      <div style={styles.overlay} />
-      <div className='overlay'>
-        <div className='modalContainer'>
-          <div style={styles.modalBlock} className='wht-txt text-center'>
+    <div className='overlay'>
+      <div className='modal-container'>
+        <div style={styles.modalBlock} className='wht-txt text-center'>
+          <div className='row'>
+            <h5 className='text-center'>
+              {/* <strong>Edit your message below:</strong> */}
+              Edit your message below:
+            </h5>
+          </div>
+          <div className='row'>
             <div className='row'>
-              <p>
-                <strong>Edit your message below:</strong>
-              </p>
-            </div>
-            <div className='row'>
-              <div className='row'>
-                <form>
-                  <diV className='row'>
-                    <label htmlFor='edit'>
-                      <input name='edit' type='text' placeholder='Message' value={message} className='edit-message' rows='2'/>
-                    </label>
-                  </diV>
-                  <div className='row text-center'>
-                    <div className='col mt-4'>
-                      <button>Cancel</button>
-                    </div>
-                    <div className='col mt-4'>
-                      <button>Confirm</button>
-                    </div>
+              <form>
+                <div className='row'>
+                  <label htmlFor='edit'>
+                    <input name='edit' type='text' placeholder='Message' value={message} className='edit-message' rows='2'
+                    onChange={e => {
+                      // console.log('modal input value: ', e.target.value);
+                      setMessage(e.target.value);
+                    }} />
+                  </label>
+                </div>
+                <div className='row'>
+                  <div className='col mt-4'>
+                    <button style={styles.buttonSize} className='cancel' >Cancel</button>
                   </div>
-                </form>
-              </div>
+                  <div className='col mt-4'>
+                    <button style={styles.buttonSize} className='confirm' >Confirm</button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
+
+  // TRIAL TWO
+  // <>
+  //   <div className='overlay' />
+  //   <div>
+  //     <div>
+  //       <div>
+  //         <p className='wht-txt' >TEST</p>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </>
+  // TRIAL ONE
+
+  // <div style={styles.modalContainer} >
+  //   <div style={styles.modalBlock}>
+  //     <p className='wht-txt overlay'>
+  //       TEST AGAIN
+  //     </p>
+  //   </div>
+  // </div>
+
   );
 }
