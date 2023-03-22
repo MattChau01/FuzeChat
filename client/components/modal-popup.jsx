f08eae211b04adde5d152b468a7bfd2b55ec7035
Matthew Chau <matthewwchau@gmail.com>
2023-03-22 00:44:56 +0000

    Create a mockup for the modal similar to the wireframe. #9

--- client/components/modal-popup.jsx
+++ client/components/modal-popup.jsx
@@ -16,13 +16,35 @@ export default function Modal(props) {
       }} >
         <div className='mt-5'>
           <div className='mt-5 pt-5'>
-            <div style={{
-              backgroundColor: '#283C46',
-              width: '20rem',
-              height: '10rem'
-            }} className='mt-5'>
-              <div>
-                <p className='wht-txt'>Are you sure you want to leave the room?</p>
+            <div className='mt-5 pt-5'>
+              <div style={{
+                backgroundColor: '#283C46',
+                width: '20rem',
+                height: '10rem'
+              }}>
+                <div className='row'>
+                  <p className='wht-txt pt-4' style={{
+                    fontWeight: 600
+                  }}>Are you sure you want to leave?</p>
+                </div>
+                <div className='row pt-3'>
+                  <div className='col'>
+                    <button style={{
+                      backgroundColor: '#EB8E8E',
+                      color: '#fff',
+                      borderRadius: '2rem',
+                      borderStyle: 'none'
+                    }}>Cancel</button>
+                  </div>
+                  <div className='col'>
+                    <button style={{
+                      backgroundColor: '#2EDF11',
+                      color: '#fff',
+                      borderRadius: '2rem',
+                      borderStyle: 'none'
+                    }}>Confirm</button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
